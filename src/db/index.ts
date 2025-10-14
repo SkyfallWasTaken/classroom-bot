9e7f770fed6a1531315343d5206a39026f374fe0
Mahad Kalam <mahadkalam1@proton.me>
2025-10-14 22:48:08 +0100

    Migrate on deploy

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,6 @@
 import { drizzle } from 'drizzle-orm/bun-sqlite';
+import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 const db = drizzle(process.env.DB_FILE_NAME!);
+migrate(db, { migrationsFolder: "./drizzle" });
 export { db };
 export { dataTable } from './schema';
